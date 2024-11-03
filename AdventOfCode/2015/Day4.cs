2cccac46fd367062818724abc668b9a421f9bcb9
Andrii Rebrin <studmm@ukr.net>
2024-11-03 15:33:21 +0100

    add 2015 4, 5

---
+++ AdventOfCode/2015/Day4.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Xunit;
+
+namespace AdventOfCode._2015;
+
+public sealed class Day4
+{
+    public int Solve1(string input) => Solve(input, "00000");
+
+    public int Solve2(string input) => Solve(input, "000000");
+
+    private static int Solve(string input, string prefix)
+    {
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            var hash = GetHash(input + i);
+            if (hash.StartsWith(prefix))
+                return i;
+        }
+
+        return int.MinValue;
+    }
+
+    private static string GetHash(string input)
+    {
+        using MD5 md5 = MD5.Create();
+
+        byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+        byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+        return Convert.ToHexString(hashBytes);
+    }
+
+    [Fact]
+    public void Test1() => Assert.Equal(254575, Solve1(Input));
+
+    [Fact]
+    public void Test2() => Assert.Equal(1038736, Solve2(Input));
+
+    private const string Input = @"bgvyzdsv";
+}
