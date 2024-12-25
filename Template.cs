1c4eb6cb3e675ec8e5ba113cf68cf227cf7aad96
Andrii Rebrin <studmm@ukr.net>
2024-12-25 17:36:15 +0100

    add day solution template

---
+++ Template.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace AdventOfCode;
+
+public sealed class Template
+{
+    public int Solve1(string input)
+    {
+        throw null;
+    }
+
+    public int Solve2(string input)
+    {
+        throw null;
+    }
+
+    [Fact]
+    public void Test1() => Assert.Equal(0, Solve1(Input));
+
+    [Fact]
+    public void Test2() => Assert.Equal(0, Solve2(Input));
+
+    private const string Input = @"";
+}
