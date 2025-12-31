fea0f04b67d4383f59b775f02f8fbd99e2a19752
Andrii Rebrin <studmm@ukr.net>
2025-12-31 07:59:12 -0800

    add 2025 day 8

---
+++ Common/UnionFind.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Immutable;
+
+namespace AdventOfCode.Common;
+
+public sealed class UnionFind
+{
+    private readonly int[] _root;
+    private readonly int[] _size;
+    private int _components;
+
+    public UnionFind(int size)
+    {
+        _root = new int[size];
+        _size = new int[size];
+
+        for(int i = 0; i < size; i++)
+        {
+            _root[i] = i;
+            _size[i] = 1;
+        }
+
+        _components = size;
+    }
+
+    public int Find( int x )
+    {
+        int root = x;
+        while (root != _root[root])
+            root = _root[root];
+
+        int current = x;
+        while( current != root)
+        {
+            int temp = _root[current];
+            _root[current] = root;
+            current = temp;
+        }
+
+        return root;
+    }
+
+    public void Union( int x, int y ) {
+        int rootX = Find(x);
+        int rootY = Find(y);
+
+        if (rootX == rootY)
+            return;
+
+        if( _size[rootX] < _size[rootY] )
+        {
+            _root[rootX] = rootY;
+            _size[rootY] += _size[rootX];
+            _size[rootX] = 0;
+
+        }
+        else
+        {
+            _root[rootY] = rootX;
+            _size[rootX] += _size[rootY];
+            _size[rootY] = 0;
+        }
+
+        _components--;
+    }
+
+    public bool Connected( int x, int y ) =>
+        Find(x) == Find(y);
+
+    public int ComponentSize( int x ) => 
+        _size[Find(x)];
+
+    public ImmutableArray<int> AllComponentSizes =>
+        [.. _size];
+
+    public int Components => 
+        _components;
+
+}
---
+++ Common/UnionFindTests.cs
@@ -0,0 +1,128 @@
+﻿namespace AdventOfCode.Common;
+
+public sealed class UnionFindTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(10)]
+    public void Constructor_Initial_State_Is_Correct(int size)
+    {
+        var uf = new UnionFind(size);
+
+        Assert.Equal(size, uf.Components);
+
+        for (int i = 0; i < size; i++)
+        {
+            Assert.True(uf.Connected(i, i));
+            Assert.Equal(1, uf.ComponentSize(i));
+        }
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    public void Union_With_Self_Is_NoOp(int a, int b)
+    {
+        var uf = new UnionFind(3);
+
+        uf.Union(a, b);
+
+        Assert.Equal(3, uf.Components);
+        Assert.Equal(1, uf.ComponentSize(a));
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    [InlineData(0, 2)]
+    public void Connected_Symmetric(int a, int b)
+    {
+        var uf = new UnionFind(3);
+
+        uf.Union(a, b);
+
+        Assert.True(uf.Connected(a, b));
+        Assert.True(uf.Connected(b, a));
+    }
+
+    [Theory]
+    [InlineData(new[] { 0, 1 }, 2)]
+    [InlineData(new[] { 0, 1, 2 }, 3)]
+    [InlineData(new[] { 1, 2, 3, 4 }, 4)]
+    public void Chain_Unions_Form_One_Component(int[] chain, int expectedSize)
+    {
+        var uf = new UnionFind(5);
+
+        for (int i = 0; i < chain.Length - 1; i++)
+            uf.Union(chain[i], chain[i + 1]);
+
+        Assert.Equal(expectedSize, uf.ComponentSize(chain[0]));
+    }
+
+    [Theory]
+    [InlineData(0, 1, 2)]
+    [InlineData(1, 2, 0)]
+    [InlineData(2, 3, 4)]
+    public void ComponentSize_Same_For_All_Members(int a, int b, int c)
+    {
+        var uf = new UnionFind(5);
+
+        uf.Union(a, b);
+        uf.Union(b, c);
+
+        var size = uf.ComponentSize(a);
+
+        Assert.Equal(size, uf.ComponentSize(b));
+        Assert.Equal(size, uf.ComponentSize(c));
+    }
+
+    [Theory]
+    [InlineData(5, 0)]
+    [InlineData(5, 1)]
+    [InlineData(5, 4)]
+    public void Find_Is_Idempotent(int size, int index)
+    {
+        var uf = new UnionFind(size);
+
+        uf.Union(0, 1);
+        uf.Union(1, 2);
+
+        var r1 = uf.Find(index);
+        var r2 = uf.Find(index);
+
+        Assert.Equal(r1, r2);
+    }
+
+    [Theory]
+    [InlineData(3, 2)]
+    [InlineData(5, 4)]
+    [InlineData(10, 9)]
+    public void Components_Equals_Size_Minus_Unions(int size, int unions)
+    {
+        var uf = new UnionFind(size);
+
+        for (int i = 0; i < unions; i++)
+            uf.Union(i, i + 1);
+
+        Assert.Equal(size - unions, uf.Components);
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    public void Repeated_Unions_Do_Not_Change_State(int a, int b)
+    {
+        var uf = new UnionFind(3);
+
+        uf.Union(a, b);
+        var componentsAfterFirst = uf.Components;
+
+        uf.Union(a, b);
+        uf.Union(b, a);
+
+        Assert.Equal(componentsAfterFirst, uf.Components);
+    }
+
+}
